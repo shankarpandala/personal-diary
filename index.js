adddc66231b744a2f2e4d9ffde61e06ab5ba6485
spandalax098461 <ShankarRao_Pandala@contractor.amat.com>
2021-01-19 13:12:07 +0530

    Completed app

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// javascript
+const entryForm = document.getElementById('entryForm');
+const entriesSection = document.querySelector('#entries');
+const entryTextbox = document.querySelector('.entry-textbox');
+const entriesNav = document.querySelector('.entries-nav');
+
+let count = 1;
+function addEntryToDom(event) {
+    event.preventDefault();
+    const entryDiv = document.createElement('div');
+    entryDiv.className = 'single-entry';
+    entryDiv.innerText = entryTextbox.value;
+    entryDiv.style.display = 'none';
+    entriesSection.appendChild(entryDiv);
+    entryTextbox.value = '';
+    
+    const displayEntryButton = document.createElement('button');
+    displayEntryButton.className = 'display-entry-button';
+    displayEntryButton.innerText = count;
+    entriesNav.appendChild(displayEntryButton);
+
+    displayEntryButton.addEventListener('click', function() {
+        const allEntries = document.querySelectorAll('.single-entry');
+        for (let index = 0; index < allEntries.length; index++) {
+            allEntries[index].style.display = 'none';
+        }
+        entryDiv.style.display = 'block';        
+    })
+    
+    count++
+}
+
+entryForm.addEventListener('submit', addEntryToDom)
